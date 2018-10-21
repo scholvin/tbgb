cc4120040b20ae91a1d29c34f00d04601675fb0e
John Scholvin <scholvin@glptrading.com>
2018-10-20 21:33:44 -0500

    basic TBGB rendering done on viz object

--- tbgb.py
+++ tbgb.py
@@ -3,9 +3,17 @@
 import Tkinter
 import viz
 
+# LEDs = 39 x 17
+
 root = Tkinter.Tk()
-v = viz.Viz(root, width=1000, height=600)
+v = viz.Viz(root)
 w = Tkinter.Label(root, text="hello world")
+for i in range(0, 39):
+    for j in range(0, 17):
+        if i % 10 == 0:
+            v.plot(i, j, 255, 255, 255)
+        else:
+            v.plot(i, j, 255, 0, 255)
 w.pack()
 root.mainloop()
 
--- viz.py
+++ viz.py
@@ -1,11 +1,70 @@
 import Tkinter
 
 class Viz(object):
-    def __init__(self, parent, height, width):
-        self.height = height
-        self.width = width
-        self.canvas = Tkinter.Canvas(parent, height=height, width=width, bg='#303030')
+    def __init__(self, parent):
+        self.height = 650
+        self.width = 1600
+        self.xoffset = 36
+        self.yoffset = 36
+        self.xscale = 36
+        self.yscale = 36
+        self.radius = 6
+        self.canvas = Tkinter.Canvas(parent, height=self.height, width=self.width, bg='#303030')
         self.canvas.pack()
+
+        # draw the T
+        self.canvas.create_line(self.xform(-0.5, -0.5), self.xform(8.5, -0.5), self.xform(8.5, 2.5),  self.xform(5.5, 2.5), \
+            self.xform(5.5, 16.5), self.xform(2.5, 16.5), self.xform(2.5, 2.5), self.xform(-0.5, 2.5), self.xform(-0.5, -0.5), \
+            fill='white')
+
+        # draw a B
+        b = 9.5
+        self.canvas.create_line(self.xform(b, -0.5), self.xform(b+7.5, -0.5), self.xform(b+9, 1), self.xform(b+9, 7.5), \
+            self.xform(b+8.5, 8.0), self.xform(b+9, 8.5), self.xform(b+9, 15), self.xform(b+7.5, 16.5), self.xform(b, 16.5), \
+            self.xform(b, -0.5),\
+            fill='white')
+        y0 = 2.5
+        self.canvas.create_line(self.xform(b+3, y0), self.xform(b+6, y0), self.xform(b+6, y0+4), self.xform(b+3, y0+4), \
+            self.xform(b+3, y0), fill='white')
+        y0 = y0 + 7
+        self.canvas.create_line(self.xform(b+3, y0), self.xform(b+6, y0), self.xform(b+6, y0+4), self.xform(b+3, y0+4), \
+            self.xform(b+3, y0), fill='white')
+
+        # draw the G
+        self.canvas.create_line(self.xform(21, -0.5), self.xform(28.5, -0.5), self.xform(28.5, 2.5), self.xform(22.5, 2.5), \
+            self.xform(22.5, 13.5), self.xform(25.5, 13.5), self.xform(25.5, 6.5), self.xform(28.5, 6.5), self.xform(28.5, 16.5), \
+            self.xform(21, 16.5), self.xform(19.5, 15.0), self.xform(19.5, 1), self.xform(21, -0.5), fill='white')
+
+
+        # draw another B
+        b = 29.5
+        self.canvas.create_line(self.xform(b, -0.5), self.xform(b+7.5, -0.5), self.xform(b+9, 1), self.xform(b+9, 7.5), \
+            self.xform(b+8.5, 8.0), self.xform(b+9, 8.5), self.xform(b+9, 15), self.xform(b+7.5, 16.5), self.xform(b, 16.5), \
+            self.xform(b, -0.5),\
+            fill='white')
+        y0 = 2.5
+        self.canvas.create_line(self.xform(b+3, y0), self.xform(b+6, y0), self.xform(b+6, y0+4), self.xform(b+3, y0+4), \
+            self.xform(b+3, y0), fill='white')
+        y0 = y0 + 7
+        self.canvas.create_line(self.xform(b+3, y0), self.xform(b+6, y0), self.xform(b+6, y0+4), self.xform(b+3, y0+4), \
+            self.xform(b+3, y0), fill='white')
+
+    def xform(self, x, y):
+        """map pixel in LED space to canvas coordinates"""
+        cx = self.xoffset + self.xscale * x
+        cy = self.yoffset + self.yscale * y
+        return cx, cy
+
+    def rgb2color(self, red, green, blue):
+        """convert (r, g, b) into a # string"""
+        hexcode = "#%02x%02x%02x" % (red, green, blue)
+        return hexcode
+
+    def plot(self, x, y, red, green, blue):
+        """plot the LED pixel at x,y (pixel space) with color r,g,b"""
+        cx, cy = self.xform(x, y)
+        r = self.radius
+        self.canvas.create_oval(cx-r, cy-r, cx+r, cy+r, fill=self.rgb2color(red, green, blue))
 
 
 
