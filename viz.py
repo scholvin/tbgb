4983388d6d316c445eda6e7351376bb857ae9dff
John Scholvin <scholvin@glptrading.com>
2018-10-20 21:56:16 -0500

    add invisible pixel list

--- viz.py
+++ viz.py
@@ -35,7 +35,6 @@ class Viz(object):
             self.xform(22.5, 13.5), self.xform(25.5, 13.5), self.xform(25.5, 6.5), self.xform(28.5, 6.5), self.xform(28.5, 16.5), \
             self.xform(21, 16.5), self.xform(19.5, 15.0), self.xform(19.5, 1), self.xform(21, -0.5), fill='white')
 
-
         # draw another B
         b = 29.5
         self.canvas.create_line(self.xform(b, -0.5), self.xform(b+7.5, -0.5), self.xform(b+9, 1), self.xform(b+9, 7.5), \
@@ -48,6 +47,38 @@ class Viz(object):
         y0 = y0 + 7
         self.canvas.create_line(self.xform(b+3, y0), self.xform(b+6, y0), self.xform(b+6, y0+4), self.xform(b+3, y0+4), \
             self.xform(b+3, y0), fill='white')
+
+    def visible(self, x, y):
+        """is pixel x, y visible?"""
+        if x >= 0 and x <= 2 and y >= 3:
+            return False
+        if x >= 6 and x <= 8 and y >= 3:
+            return False
+        if x == 9:
+            return False
+        if x >= 13 and x <= 15 and y >= 3 and y <= 6:
+            return False
+        if x >= 13 and x <= 15 and y >= 10 and y <= 13:
+            return False
+        if (x == 18 or x == 20) and (y == 0 or y == 16):
+            return False
+        if (x == 18 and y == 8):
+            return False
+        if (x == 19):
+            return False
+        if (x >= 23 and x <= 25) and (y >= 3 and y <= 13):
+            return False
+        if (x >= 26 and x <= 28 and (y >= 3 and y <= 6)):
+            return False
+        if (x == 29):
+            return False
+        if x >= 33 and x <= 35 and y >= 3 and y <= 6:
+            return False
+        if x >= 33 and x <= 35 and y >= 10 and y <= 13:
+            return False
+        if x == 38 and (y == 0 or y == 8 or y == 16):
+            return False;
+        return True
 
     def xform(self, x, y):
         """map pixel in LED space to canvas coordinates"""
@@ -62,6 +93,8 @@ class Viz(object):
 
     def plot(self, x, y, red, green, blue):
         """plot the LED pixel at x,y (pixel space) with color r,g,b"""
+        if not self.visible(x, y):
+            return
         cx, cy = self.xform(x, y)
         r = self.radius
         self.canvas.create_oval(cx-r, cy-r, cx+r, cy+r, fill=self.rgb2color(red, green, blue))
