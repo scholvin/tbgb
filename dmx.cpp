c2394e90cbc9f6ace264a193672f9a27cd962136
John Scholvin <scholvin@glptrading.com>
2018-11-13 07:21:57 -0600

    design layout (see google sheet) and then code out data structures for DMX translation. Still have to write mapping code in render().

--- dmx.cpp
+++ dmx.cpp
@@ -1,15 +1,145 @@
 #include <ola/Logging.h>
 #include <ola/client/StreamingClient.h>
+#include <array>
+#include <set>
 #include "dmx.h"
+#include "global.h"
+
+namespace {
+
+const int TCOUNT = 69;
+const int BCOUNT = 126;
+const int GCOUNT = 106;
+
+// irritating: x-y nature is reversed into row-col here
+const int Tdef[TBGB_YMAX][LETTER_WIDTH] = {
+    { 60, 61, 62, 63, 64, 65, 66, 67, 68 },
+    { 59, 58, 57, 56, 55, 54, 53, 52, 51 },
+    { 42, 43, 44, 45, 46, 47, 48, 49, 50 },
+    { -1, -1, -1, 41, 40, 39, -1, -1, -1 },
+    { -1, -1, -1, 36, 37, 38, -1, -1, -1 },
+    { -1, -1, -1, 35, 34, 33, -1, -1, -1 },
+    { -1, -1, -1, 30, 31, 32, -1, -1, -1 },
+    { -1, -1, -1, 29, 28, 27, -1, -1, -1 },
+    { -1, -1, -1, 24, 25, 26, -1, -1, -1 },
+    { -1, -1, -1, 23, 22, 21, -1, -1, -1 },
+    { -1, -1, -1, 18, 19, 20, -1, -1, -1 },
+    { -1, -1, -1, 17, 16, 15, -1, -1, -1 },
+    { -1, -1, -1, 12, 13, 14, -1, -1, -1 },
+    { -1, -1, -1, 11, 10,  9, -1, -1, -1 },
+    { -1, -1, -1,  6,  7,  8, -1, -1, -1 },
+    { -1, -1, -1,  5,  4,  3, -1, -1, -1 },
+    { -1, -1, -1,  0,  1,  2, -1, -1, -1 }
+};
+
+const int Bdef[TBGB_YMAX][LETTER_WIDTH] = {
+    { 118, 119, 120, 121, 122, 123, 124, 125,  -1 },
+    { 117, 116, 115, 114, 113, 112, 111, 110, 109 },
+    {  88,  89,  90,  91,  92,  93,  94, 107, 108 },
+    {  87,  86,  85,  -1,  -1,  -1,  95, 106, 105 },
+    {  82,  83,  84,  -1,  -1,  -1,  96, 103, 104 },
+    {  81,  80,  79,  -1,  -1,  -1,  97, 102, 101 },
+    {  76,  77,  78,  -1,  -1,  -1,  98,  99, 100 },
+    {  75,  74,  73,  72,  71,  70,  69,  68,  67 },
+    {  59,  60,  61,  62,  63,  64,  65,  66,  -1 },
+    {  58,  57,  44,  43,  42,  41,  40,  39,  38 },
+    {  55,  56,  45,  -1,  -1,  -1,  35,  36,  37 },
+    {  54,  53,  46,  -1,  -1,  -1,  34,  33,  32 },
+    {  51,  52,  47,  -1,  -1,  -1,  29,  30,  31 },
+    {  50,  49,  48,  -1,  -1,  -1,  28,  27,  26 },
+    {  17,  18,  19,  20,  21,  22,  23,  24,  25 },
+    {  16,  15,  14,  13,  12,  11,  10,   9,   8 },
+    {   0,   1,   2,   3,   4,   5,   6,   7,  -1 }
+};
+
+const int Gdef[TBGB_YMAX][LETTER_WIDTH] = {
+    {  -1,  86,  87,  88,  89,  90,  91,  92,  93 },
+    {  83,  84,  85,  99,  98,  97,  96,  95,  94 },
+    {  82,  81,  80, 100, 101, 102, 103, 104, 105 },
+    {  77,  78,  79,  -1,  -1,  -1,  -1,  -1,  -1 },
+    {  76,  75,  74,  -1,  -1,  -1,  -1,  -1,  -1 },
+    {  71,  72,  73,  -1,  -1,  -1,  -1,  -1,  -1 },
+    {  70,  69,  68,  -1,  -1,  -1,  -1,  -1,  -1 },
+    {  65,  66,  67,  -1,  -1,  -1,   0,   1,   2 },
+    {  64,  63,  62,  -1,  -1,  -1,   5,   4,   3 },
+    {  59,  60,  61,  -1,  -1,  -1,   6,   7,   8 },
+    {  58,  57,  56,  -1,  -1,  -1,  11,  10,   9 },
+    {  53,  54,  55,  -1,  -1,  -1,  12,  13,  14 },
+    {  52,  51,  50,  -1,  -1,  -1,  17,  16,  15 },
+    {  47,  48,  49,  -1,  -1,  -1,  18,  19,  20 },
+    {  46,  45,  44,  43,  42,  41,  40,  39,  21 },
+    {  31,  32,  33,  34,  35,  36,  37,  38,  22 },
+    {  -1,  30,  29,  28,  27,  26,  25,  24,  23 }
+};
+
+void check_grids()
+{
+    // check to make sure data structures aren't typoed
+    std::set<int> check;
+    for (auto row = 0; row < TBGB_YMAX; row++)
+    {
+        for (auto col = 0; col < LETTER_WIDTH; col++)
+        {
+            auto v = Tdef[row][col];
+            if (v == -1)
+                continue;
+            if (v < -1 || v >= TCOUNT)
+                throw std::logic_error("unexpected value in T " + std::to_string(v));
+            if (check.find(v) != check.end())
+                throw std::logic_error("found duplicate in T " + std::to_string(v));
+            check.insert(v);
+        }
+    }
+    for (auto i = 0; i < TCOUNT; i++)
+        if (check.find(i) == check.end())
+            throw std::logic_error("missing value in T " + std::to_string(i));
+    check.clear();
+    for (auto row = 0; row < TBGB_YMAX; row++)
+    {
+        for (auto col = 0; col < LETTER_WIDTH; col++)
+        {
+            auto v = Bdef[row][col];
+            if (v == -1)
+                continue;
+            if (v < -1 || v >= BCOUNT)
+                throw std::logic_error("unexpected value in B " + std::to_string(v));
+            if (check.find(v) != check.end())
+                throw std::logic_error("found duplicate in B " + std::to_string(v));
+            check.insert(v);
+        }
+    }
+    for (auto i = 0; i < BCOUNT; i++)
+        if (check.find(i) == check.end())
+            throw std::logic_error("missing value in B " + std::to_string(i));
+    check.clear();
+    for (auto row = 0; row < TBGB_YMAX; row++)
+    {
+        for (auto col = 0; col < LETTER_WIDTH; col++)
+        {
+            auto v = Gdef[row][col];
+            if (v == -1)
+                continue;
+            if (v < -1 || v >= GCOUNT)
+                throw std::logic_error("unexpected value in G " + std::to_string(v));
+            if (check.find(v) != check.end())
+                throw std::logic_error("found duplicate in G " + std::to_string(v));
+            check.insert(v);
+        }
+    }
+    for (auto i = 0; i < GCOUNT; i++)
+        if (check.find(i) == check.end())
+            throw std::logic_error("missing value in G " + std::to_string(i));
+}
+}
 
 DMX::DMX(Framebuf& fb) : m_fb(fb)
 {
-    ola::InitLogging(ola::OLA_LOG_WARN, ola::OLA_LOG_STDERR); // stdout not available
+    check_grids();
+    ola::InitLogging(ola::OLA_LOG_WARN,   ola::OLA_LOG_STDERR); // stdout not available
     m_buffer.Blackout();
     m_ola_client = new ola::client::StreamingClient(ola::client::StreamingClient::Options());
     if (!m_ola_client->Setup())
         throw std::runtime_error("Could not setup ola client");
-
 }
 
 DMX::~DMX()
--- global.h
+++ global.h
@@ -1,5 +1,6 @@
 #pragma once
 
-static const int TBGB_XMAX = 39;
+static const int LETTER_WIDTH = 9;
+static const int TBGB_XMAX = LETTER_WIDTH * 4 + 3; // 1 column between each;
 static const int TBGB_YMAX = 17;
 
